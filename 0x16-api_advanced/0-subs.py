ad55fedb08ba7196b49c8863616ac502961dd42a
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-09-03 04:56:01 +0000

    typo deleted

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,11 +6,11 @@ import requests
 def number_of_subscribers(subreddit):
     ''' Returns the number of subscribers for a given subreddit.
     If an invalid subreddit is given returns 0 '''
-    my_url = "https://www.reddit.com/r/{}/about".format(subreddit)
-    headers = {"My User-Agent": "My User Agent Request"}
+    my_url = "https://api.reddit.com/r/{}/about".format(subreddit)
+    headers = {"User-Agent": "My User Agent Request"}
 
     try:
         get_url = requests.get(my_url, headers=headers).json()
-        return get_url['data']['subscribers']
+        return get_url["data"]["subscribers"]
     except:
         return 0
