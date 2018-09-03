0d5c751c5ed4f7e96ce7e73b1171d9a028e3f730
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-09-03 06:20:44 +0000

    recurcion termination added

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,13 +9,14 @@ def recurse(subreddit, hot_list=[], after=""):
 
     m_url = "https://api.reddit.com/r/{}/hot?after={}".format(subreddit, after)
     headers = {"User-Agent": "My User Agent Request"}
-
     try:
         get_url = requests.get(m_url, headers=headers).json()
         children = get_url.get("data").get("children")
         for child in children:
             hot_list.append(child.get("data").get("title"))
         after = get_url.get("data").get("after")
+        if after is None:
+            return hot_list
         return recurse(subreddit, hot_list, after)
     except:
         return None
