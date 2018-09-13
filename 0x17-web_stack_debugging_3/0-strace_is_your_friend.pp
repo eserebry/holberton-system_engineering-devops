cb08ceee438590b42304162b1803b6aa652182a9
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-09-13 04:43:35 +0000

    path specified

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # find out why Apache is returning a 500 error
 exec { 'fix_the_prosecc':
-  command => 'sed -i -e "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
-  provider => 'shell'
+  path => '/usr/bin:/usr/sbin:/bin',
+  provider => 'shell',
+  command => 'sed -i -e "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php'
 }
