20e0896bf5ca018d8e08ca5302f54aa790ef037f
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-09-13 04:51:58 +0000

    fixed puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # find out why Apache is returning a 500 error
 exec { 'fix_the_prosecc':
-  path => '/usr/bin:/usr/sbin:/bin',
+  path     => '/usr/bin:/usr/sbin:/bin',
   provider => 'shell',
-  command => 'sed -i -e "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php'
+  command  => 'sed -i -e "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php'
 }
